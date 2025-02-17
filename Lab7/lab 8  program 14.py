95425d488f66891f1f6e699d8240125015eb13fd
Cyber00924 <mittalt071@gmail.com>
2025-02-17 20:00:53 +0530

    Add files via upload

---
+++ Lab7/lab 8  program 14.py
@@ -0,0 +1,3 @@
+my_tuple = (10, 20, 30, 40, 50)
+index_of_item = my_tuple.index(30)
+print(index_of_item)
---
+++ Lab7/lab 8 program 10.py
@@ -0,0 +1,6 @@
+my_tuple = (10, 20, 30, 40, 50)
+element = 30
+if element in my_tuple:
+    print("Element exists in the tuple.")
+else:
+    print("Element does not exist in the tuple.")
